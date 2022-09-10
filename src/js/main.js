beedf995344d2817fc863dce99697c03b1e8a9ec
Aymeric Laurent--Guenver <aymeric.laurent-guenver@atos.net>
2022-09-10 13:52:24 +0200

    init project

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+const BASE_URL = window.location.protocol + '//' + window.location.host;
+
+document.addEventListener('DOMContentLoaded', () => {
+	// PARTIALS
+	@@include('partial/watermark.js')
+});
